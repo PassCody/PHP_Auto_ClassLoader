46d34734b9aa90c67ab24368ba26c887d996bed5
PassCody <44557819+PassCody@users.noreply.github.com>
2023-08-15 12:58:50 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="robots" content="noindex,nofollow" />
+		<title>
+			Dev-System
+		</title>
+		<link rel="shortcut icon" href="./src/imgs/favicon-icon.png">
+		<link rel="stylesheet" type="text/css" href="./src/css/main.css" />
+	</head>
+	<body>
+		<?php
+			include_once($init->getMainDir()."src/tools/php/ClassLoader.php");
+			$cl = new ClassLoader();
+			$cl->printPath($init->getMainDir());
+			print_r(array_keys($cl->getObjects()));
+			$cl->getObjects()["sl"]->sessionStart();
+			echo("<br>");
+			foreach (getallheaders() as $key => $val) {
+				echo($key . " => " . $val . "<br>");
+			}
+		?>
+	</body>
+</html>
